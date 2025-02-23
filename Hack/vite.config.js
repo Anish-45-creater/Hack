2504b86133c3aae6b13ded3fab9f2a0a67c82eb9
Krishna M <107485712+Krishna-2004@users.noreply.github.com>
2025-02-23 17:34:04 +0530

    Update vite.config.js

--- Hack/vite.config.js
+++ Hack/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: './', // Ensures correct paths
   build: {
     outDir: 'dist', // Ensures correct build output
